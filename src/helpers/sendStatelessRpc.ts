a157284dd33bdd47f77b6c6dea3c23547f565aac
William Nemencha <william@ethicdevs.com>
2022-09-14 15:45:30 +0200

    release(1.4.3): fix the sideband so it works

--- src/helpers/sendStatelessRpc.ts
+++ src/helpers/sendStatelessRpc.ts
@@ -36,74 +36,84 @@ export function sendStatelessRpc({
 }) {
   const spawnGitCmd = (args: string[]) => spawnGit(opts, args, cwd);
   return new Promise((resolve, reject) => {
-    if (opts.withSideBandMessages === true && opts.onPush != null) {
-      let onPushPerfomedAction: boolean = false;
-      let shouldAcceptRequest: boolean = false;
-      let denyReason: null | string = null;
-
-      const accept = () => {
-        onPushPerfomedAction = true;
-        shouldAcceptRequest = true;
-        logTrace("accept was called.");
-      };
-
-      const deny = (reason: string) => {
-        onPushPerfomedAction = true;
-        shouldAcceptRequest = false;
-        denyReason = reason;
-        logTrace("deny was called. reason:", reason);
-      };
-
-      // protect against miss-implemented onPush by automatically denying the
-      // request in case onPush hook does not reply after 30 seconds.
-      let autoDenyTimeoutId: null | NodeJS.Timer = setTimeout(() => {
-        if (autoDenyTimeoutId == null) {
-          return undefined;
-        }
-        if (autoDenyTimeoutId != null) {
-          clearTimeout(autoDenyTimeoutId);
-          autoDenyTimeoutId = null;
-        }
-        return deny(
-          `onPush did not respond within the allowed ${ON_PUSH_TIMEOUT_MS} seconds.`,
-        );
-      }, ON_PUSH_TIMEOUT_MS);
-
-      // ask client for resolution about what to do + possibility to write to
-      // the side band messages stream.
-      opts.onPush({
-        type: "push",
-        message: new GitServerMessage(gitStream, accept, deny),
-        data: {
-          packType,
-          repoDiskPath: cwd,
-          repoSlug,
-          request,
-          requestMethod,
-          requestType,
-          username,
-        },
-      });
-
-      clearTimeout(autoDenyTimeoutId);
-      autoDenyTimeoutId = null;
-
-      if (onPushPerfomedAction === false) {
-        accept();
-      }
-
-      if (shouldAcceptRequest === false && denyReason != null) {
-        gitStream.end();
-        return reject(new Error(denyReason));
-      }
-    }
-
     const safePackType = safeServiceToPackType(packType);
     const process = spawnGitCmd([safePackType, GIT_STATELESS_RPC_FLAG]);
 
     request.raw.pipe(process.stdin, { end: false });
 
     process.stdout.on("data", (chunk) => gitStream.write(chunk));
+
+    process.stdout.once("data", () => {
+      if (opts.withSideBandMessages === true && opts.onPush != null) {
+        let onPushPerfomedAction: boolean = false;
+        let shouldAcceptRequest: boolean = false;
+        let denyReason: null | string = null;
+
+        const accept = () => {
+          onPushPerfomedAction = true;
+          shouldAcceptRequest = true;
+          logTrace("accept was called.");
+        };
+
+        const deny = (reason: string) => {
+          onPushPerfomedAction = true;
+          shouldAcceptRequest = false;
+          denyReason = reason;
+          logTrace("deny was called. reason:", reason);
+        };
+
+        // protect against miss-implemented onPush by automatically denying the
+        // request in case onPush hook does not reply after 30 seconds.
+        let autoDenyTimeoutId: null | NodeJS.Timer = setTimeout(() => {
+          if (autoDenyTimeoutId == null) {
+            return undefined;
+          }
+          if (autoDenyTimeoutId != null) {
+            clearTimeout(autoDenyTimeoutId);
+            autoDenyTimeoutId = null;
+          }
+          return deny(
+            `onPush did not respond within the allowed ${ON_PUSH_TIMEOUT_MS} seconds.`,
+          );
+        }, ON_PUSH_TIMEOUT_MS);
+
+        // ask client for resolution about what to do + possibility to write to
+        // the side band messages stream.
+        opts.onPush({
+          type: "push",
+          message: new GitServerMessage(gitStream, accept, deny),
+          data: {
+            packType,
+            repoDiskPath: cwd,
+            repoSlug,
+            request,
+            requestMethod,
+            requestType,
+            username,
+          },
+        });
+
+        clearTimeout(autoDenyTimeoutId);
+        autoDenyTimeoutId = null;
+
+        if (onPushPerfomedAction === false) {
+          accept();
+        }
+
+        if (shouldAcceptRequest === false && denyReason != null) {
+          process.kill();
+          gitStream.end();
+          return reject(new Error(denyReason));
+        }
+
+        // else just let the command complete
+        return undefined;
+      }
+
+      // do nothing
+      return undefined;
+    });
+
     process.stdout.on("close", () => resolve(gitStream.end()));
   });
 }
